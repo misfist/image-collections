2108b2a38a9b48f06f5ff5b8830a67673e2a923a
Pea <pea@misfist.com>
2015-08-18 14:41:01 -0400

    Switched artv2 and art

--- artv2.php
+++ artv2.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<?php include_once('headv2.php'); ?>
+<?php include_once('head.php'); ?>
 
 
 <body class="proj-art" data-collection="art">
@@ -7,21 +7,58 @@
 
         <?php include_once('header.php'); ?>
 
-        <div class="container">
 
-        	<main>
-        		<div id="feature-image" class="left-img">
-					<!-- <img src="images/cahuita-1.jpg" width="747" height="500"> -->
 
-					<?php 
-		               	$url = 'data/data.json';
-						$content = file_get_contents($url);
-						$json = json_decode($content, true);
+        <div class="proj-wrapper">
+        	<div class="arrow-left"></div> <!-- arrow-left close -->
 
-						// CHANGE THIS TO COLLECTION NAME (e.g. $json['beach'])
-						$collection = $json['art'];
+		    <div class="left">
+					<div id="feature-image" class="left-img owl-carousel">
+						<!-- <img src="images/cahuita-1.jpg" width="747" height="500"> -->
 
-						$feature_content = '';
+						<?php 
+			               	$url = 'data/data.json';
+							$content = file_get_contents($url);
+							$json = json_decode($content, true);
+
+							// CHANGE THIS TO COLLECTION NAME (e.g. $json['beach'])
+							$collection = $json['art'];
+
+							$feature_content = '';
+
+							for( $i = 0; $i < count($collection); $i++ ) {
+
+								$title = $collection[$i]['title'];
+								$path = $collection[$i]['path'];
+								$description = $collection[$i]['description'];
+
+								$feature_content .= '<li>';
+								$feature_content .= '<img src="' . $path . '" data-hash="' . $i . '" alt="' . $title . '">';
+								$feature_content .= '<div id="collection-description" class="left-text"><p>' . $description . '</p></div>';
+								$feature_content .= '</li>';
+
+							}
+
+							echo $feature_content;
+						?>
+
+					</div> <!-- left-img close -->
+
+					<div id="collection-description" class="left-text"></div> <!-- left-text close -->
+		    </div> <!-- left close -->
+
+		    <div class="arrow-right"></div> <!-- arrow-right close -->
+
+			<div class="right">
+
+		        <div id="scrollbar1">
+		        	<a class="buttons prev" href="#"><img src="images/arrow-up.png" height="18" width="31"></a>
+			        <div class="viewport">
+		               <ul id="image-list" class="overview" data-collection="art">
+
+		               	<?php
+
+		               	$thumbnail_content = '';
 
 						for( $i = 0; $i < count($collection); $i++ ) {
 
@@ -29,45 +66,25 @@
 							$path = $collection[$i]['path'];
 							$description = $collection[$i]['description'];
 
-							$feature_content .= '<div class="image-item">';
-							$feature_content .= '<img src="' . $path . '" data-hash="' . $i . '" alt="' . $title . '">';
-							$feature_content .= '<div id="collection-description" class="left-text"><p>' . $description . '</p></div>';
-							$feature_content .= '</div>';
+							$thumbnail_content .= '<li><a href="#' . $i . '">';
+							$thumbnail_content .= '<img src="' . $path . '" alt="' . $title . '" class="thumbnail">';
+							$thumbnail_content .= '</a></li>';
 
 						}
 
-						echo $feature_content;
-					?>
+						echo $thumbnail_content;
 
-				</div> <!-- left-img close -->
-        	</main>
-        	<aside>
-				<div id="image-list" data-collection="art">
+		               	?>
+		               	
+		                </ul> <!-- overview close -->
+			        </div> <!-- viewport close -->
+			        <a class="buttons next" href="#"><img src="images/arrow-down.png" height="18" width="31"></a>
+		        </div> <!-- scrollbar1 close -->
 
-				<?php
+		    </div> <!-- right close -->
 
-					$thumbnail_content = '';
 
-					for( $i = 0; $i < count($collection); $i++ ) {
-
-						$title = $collection[$i]['title'];
-						$path = $collection[$i]['path'];
-						$description = $collection[$i]['description'];
-
-						$thumbnail_content .= '<div class="image-thumb"><a href="#' . $i . '">';
-						$thumbnail_content .= '<img src="' . $path . '" alt="' . $title . '" class="thumbnail">';
-						$thumbnail_content .= '</a></div>';
-
-					}
-
-					echo $thumbnail_content;
-
-				?>
-
-				</div> <!-- overview close -->
-        	</aside>
-
-        </div>
+        </div> <!-- proj-wrapper close -->      
 
 
         <?php include_once('footer.php'); ?>
@@ -77,9 +94,36 @@
 
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<!-- <script src="assets/js/app.js"></script> -->
+<script src="assets/js/owl.carousel.js"></script>
 
-<script type="text/javascript" src="assets/js/slick.js"></script>
-<script type="text/javascript" src="assets/js/main.js"></script>
+<script>
+	$('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        center:true,
+        margin:10,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash',
+        touchDrag: false,
+        nav: true,
+        navText: ['<img src="images/arrow-left.png" height="20" width="12">', '<img src="images/arrow-right.png" height="20" width="12">']
+    });
+</script>
+
+<script src="assets/js/jquery.tinycarousel.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$( '#scrollbar1' ).tinycarousel({
+			axis: "y",
+			infinite: false
+		});
+	});
+</script>
 
 
 </body>
